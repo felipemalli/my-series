1e7b276bf1a3e0bc844d1462a53cde815a26e46f
felipemalli <felipevahia@gmail.com>
2022-10-09 22:40:32 -0300

    feat: implement models

--- src/main/java/com/trybe/acc/java/minhasseries/model/Episodio.java
+++ src/main/java/com/trybe/acc/java/minhasseries/model/Episodio.java
@@ -1,9 +1,77 @@
 package com.trybe.acc.java.minhasseries.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Episodio {
 
+  /**
+   * Atributos.
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+  private int numero;
+  private int duracaoEmMinutos;
+  @ManyToOne
+  @JoinColumn(name = "serie_id")
+  private Serie serie;
 
+  /**
+   * Construtores.
+   */
+  public Episodio() {
+  }
+
+  public Episodio(int numero, int duracaoEmMinutos) {
+    this.numero = numero;
+    this.duracaoEmMinutos = duracaoEmMinutos;
+  }
+
+  public Episodio(Integer id, int numero, int duracaoEmMinutos, Serie serie) {
+    this.id = id;
+    this.numero = numero;
+    this.duracaoEmMinutos = duracaoEmMinutos;
+    this.serie = serie;
+  }
+
+  /**
+   * Métodos.
+   */
+  public Integer getId() {
+    return id;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getDuracaoEmMinutos() {
+    return duracaoEmMinutos;
+  }
+
+  public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+    this.duracaoEmMinutos = duracaoEmMinutos;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    if (!(obj instanceof Episodio)) {
+      return false;
+    }
+    Episodio episodio = (Episodio) obj;
+    return episodio.numero == this.numero;
+  }
 }
--- src/main/java/com/trybe/acc/java/minhasseries/model/Serie.java
+++ src/main/java/com/trybe/acc/java/minhasseries/model/Serie.java
@@ -1,9 +1,58 @@
 package com.trybe.acc.java.minhasseries.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Serie {
 
+  /**
+   * Atributos.
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+  private String nome;
+  @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+  private List<Episodio> episodios = new ArrayList<>();
+
+  /**
+   * Construtores.
+   */
+  public Serie() {
+  }
+
+  public Serie(String nome) {
+    this.nome = nome;
+  }
+
+  /**
+   * Métodos.
+   */
+  public Integer getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public List<Episodio> getEpisodios() {
+    return episodios;
+  }
+
+  public void adicionarEpisodio(Episodio episodio) {
+    this.episodios.add(episodio);
+  }
 
 }
